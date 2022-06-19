8ec0a47e3e9ee375fc2224a8cf2eee2556a85afc
AbuTalha <salim.meetme@gmail.com>
2022-06-19 08:37:30 +0300

    Initial Commit

---
+++ Shared/ReadAloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadAloudApp.swift
+//  Shared
+//
+//  Created by AbuTalha on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadAloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
